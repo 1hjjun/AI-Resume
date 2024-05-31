74b5aee635e98673e9c0af9bde18ff730b7c9333
1hjjun <aut7410@gmail.com>
2024-05-31 13:57:02 +0900

    0531

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <>
       <section className="intro">
-          <h1>Hyungjun Han</h1>
-          <h2>Cloud security expert</h2>
-          <p>안녕하세요.<br></br>
-            클라우드 보안 전문가 한형준입니다.</p>
-          <div>
-            <button>View Portfolio</button>
-            <button>AI Resume</button>
-          </div>
+        <h1>Hyungjun Han</h1>
+        <h2>Cloud security expert</h2>
+        <p>안녕하세요.<br></br>
+          클라우드 보안 전문가 한형준입니다.</p>
+        <div>
+          <button>View Portfolio</button>
+          <button>AI Resume</button>
+        </div>
       </section>
     </>
   );
