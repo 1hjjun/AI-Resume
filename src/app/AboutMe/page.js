707e0f423fada79debb5aa37412cb85225eee586
1hjjun <aut7410@gmail.com>
2024-05-31 13:47:29 +0900

    0531

---
+++ src/app/AboutMe/page.js
@@ -0,0 +1,33 @@
+import RootLayout from "../layout";
+
+export default function AboutMe () {
+  return (
+    <RootLayout>
+			<>
+				<section className="about-me">
+					<h1>이름 : Hyungjun Han</h1>
+					<h2>Cloud security expert</h2>
+					<p>
+						안녕하세요.<br />
+						클라우드 보안 전문가 한형준입니다.<br />
+						이름 : 한형준<br />
+						나이 : 25<br />
+						성별 : 남<br />
+						주소 : 부산광역시 금강로 319 301호<br />
+						연락처 : 010-9135-3990<br />
+						링크드인 : <a href="https://www.linkedin.com/in/형준-한-aa46b32a8" target="_blank" rel="noopener noreferrer">www.linkedin.com/in/형준-한-aa46b32a8</a><br />
+						velog : <a href="https://velog.io/@aut7410/posts" target="_blank" rel="noopener noreferrer">https://velog.io/@aut7410/posts</a><br />
+						학위 : 부산대학교 조선해양공학과 졸 (부전공 정보컴퓨터공학과)<br />
+						스킬 : HTML, CSS, Javascript, JAVA Spring<br />
+						자격증 : 정보처리기사, AWS ASS, Azure 800, oPIC<br />
+						경력 : 무<br />
+						교육과정 : AI스쿨 알토르 12주, 리팩토링 40주<br />
+						취미 : 농구<br />
+						대인관계 : 농구동아리 회장 경험<br />
+						일 경험 : 과외 10명 교육 경험<br />
+					</p>
+				</section>
+			</>
+    </RootLayout>
+  );
+}
